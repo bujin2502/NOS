01eafbdc5fab64cf980f12e96593f2f5c6bf6b7e
Zlatko Pračić <53391918+bujin2502@users.noreply.github.com>
2023-11-06 21:12:51 +0100

    Update pracic.py

--- pracic.py
+++ pracic.py
@@ -1,8 +1,8 @@
 import hashlib
+import os
 from tkinter import *
 from tkinter import filedialog
 import tkinter.messagebox as mbox
-from click import Option
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat, PrivateFormat, NoEncryption, load_pem_public_key, load_pem_private_key
@@ -12,6 +12,7 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from cryptography.exceptions import InvalidSignature
 import base64
+
 
 def generate_rsa_keys():
     private_key = rsa.generate_private_key(
@@ -34,11 +35,14 @@ def generate_rsa_keys():
         ))
     mbox.showinfo(title="RSA ključevi", message="RSA ključevi su kreirani", **{"default": mbox.OK})
 
+
 def generate_aes_key():
     key = get_random_bytes(32)
     key_file_path = "tajni_kljuc.txt"
     with open(key_file_path, "wb") as key_file:
         key_file.write(key)
+    mbox.showinfo(title="AES ključ", message="AES ključ je kreiran", **{"default": mbox.OK})
+
 
 def encrypt_asymmetric_rsa(file_path):
     with open(file_path, "rb") as f:
@@ -62,7 +66,9 @@ def encrypt_asymmetric_rsa(file_path):
     with open(file_path + ".enc", "wb") as f:
         f.write(encrypted_data)
 
-    print(f"Encryption complete. Encrypted file: {file_path}.enc")
+    ime_datoteke = os.path.basename(file_path)
+    poruka = f"Šifrirani dokument: {ime_datoteke}"
+    mbox.showinfo(title="Šifriranje javnim ključem", message=poruka + ".enc", **{"default": mbox.OK})
 
 
 def decrypt_asymmetric_rsa(file_path):
@@ -88,7 +94,10 @@ def decrypt_asymmetric_rsa(file_path):
     with open(file_path[:-4], "wb") as f:
         f.write(decrypted_data)
 
-    print(f"Decryption complete. Decrypted file: {file_path[:-4]}")
+    ime_datoteke = os.path.basename(file_path[:-4])
+    poruka = f"Dešifrirani dokument: {ime_datoteke}"
+    mbox.showinfo(title="Dešifriranje privatnim ključem", message=poruka, **{"default": mbox.OK})
+
 
 def encrypt_symmetric_aes_with_existing_key(file_path, key_path):
     with open(key_path, "rb") as key_file:
@@ -106,7 +115,10 @@ def encrypt_symmetric_aes_with_existing_key(file_path, key_path):
     with open(encrypted_file_path, "wb") as f:
         [f.write(x) for x in (cipher.nonce, tag, encrypted_data)]
 
-    print(f"Encryption complete. Encrypted file: {encrypted_file_path}")
+    ime_datoteke = os.path.basename(encrypted_file_path)
+    poruka = f"Šifrirani dokument: {ime_datoteke}"
+    mbox.showinfo(title="Simetrično šifriranje", message=poruka, **{"default": mbox.OK})
+
 
 def decrypt_symmetric_aes_with_existing_key(file_path, key_path):
     with open(file_path, "rb") as f:
@@ -122,25 +134,33 @@ def decrypt_symmetric_aes_with_existing_key(file_path, key_path):
     with open(file_path[:-4], "wb") as f:
         f.write(decrypted_data)
 
-    print(f"Decryption complete. Decrypted file: {file_path[:-4]}")
+    ime_datoteke = os.path.basename(file_path[:-4])
+    poruka = f"Dešifrirani dokument: {ime_datoteke}"
+    mbox.showinfo(title="Simetrično dešifriranje", message=poruka, **{"default": mbox.OK})
+
 
 def create_hash(file_path):
-    try:
+    if os.path.isfile(file_path):
+        file_name = os.path.basename(file_path)
+        
+        hash_value = hashlib.sha256()
         with open(file_path, 'rb') as file:
-            data = file.read()
-            sha256_hash = hashlib.sha256(data).hexdigest()
-            return sha256_hash
-    except Exception as e:
-        return f"Error: {str(e)}"
+            while True:
+                data = file.read(65536)
+                if not data:
+                    break
+                hash_value.update(data)
+        
+        hash_hex = hash_value.hexdigest()
 
-def save_hash_to_file(file_path):
-    hash_value = create_hash(file_path)
-    if hash_value and not hash_value.startswith("Error"):
-        with open("hash.txt", 'w') as hash_file:
-            hash_file.write(hash_value)
-        return f"SHA-256 Hash saved to 'hash.txt': {hash_value}"
-    else:
-        return hash_value
+        new_file_name = os.path.splitext(file_name)[0] + ".hash.txt"
+
+        with open(new_file_name, 'w') as hash_file:
+            hash_file.write(hash_hex)
+
+    poruka = f"SHA-256 Hash spremljen u datoteku: {new_file_name}"
+    mbox.showinfo(title="Sažimanje datoteke", message=poruka)
+
 
 def sign_file(file_path):
     with open('privatni_kljuc.txt', 'rb') as key_file:
@@ -153,7 +173,6 @@ def sign_file(file_path):
     with open(file_path, 'rb') as f:
         payload = f.read()
 
-    # Sign the payload file.
     signature = base64.b64encode(
         private_key.sign(
             payload,
@@ -167,7 +186,10 @@ def sign_file(file_path):
     with open(file_path + ".sig", 'wb') as f:
         f.write(signature)
 
-    print(f"Signing complete. Signature file: {file_path}.sig")
+    ime_datoteke = os.path.basename(file_path)
+    poruka = f"Potpisivanje dokumenta {ime_datoteke} dovršeno. Kreiran je potpis {ime_datoteke}.enc"
+    mbox.showinfo(title="Potpis dokumenta", message=poruka)
+
 
 def verify_signature(public_key_path, signature_path, original_file_path):
     with open(public_key_path, 'rb') as f:
@@ -189,9 +211,10 @@ def verify_signature(public_key_path, signature_path, original_file_path):
             ),
             hashes.SHA256(),
         )
-        print('Signature is valid.')
+        mbox.showinfo(title="Provjera potpisa", message="Potpis je valjan")
     except InvalidSignature as e:
-        print('ERROR: Signature is NOT valid!')
+        mbox.showwarning(title="Provjera potpisa", message="Potpis nije valjan")
+
 
 root = Tk()
 root.title("Data Protector by bujin")
@@ -202,7 +225,7 @@ for i in range(3):
 asymmetric_frame = LabelFrame(root, text="Asimetrično šifriranje - RSA")
 asymmetric_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
 
-generate_keys_button = Button(asymmetric_frame, text="Kreirati RSA ključeve", command=generate_rsa_keys)
+generate_keys_button = Button(asymmetric_frame, text="Kreiraj RSA ključeve", command=generate_rsa_keys)
 generate_keys_button.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
 asymmetric_file_label = Label(asymmetric_frame, text="Odaberi datoteku za šifriranje/dešifriranje:")
@@ -226,7 +249,7 @@ asymmetric_decrypt_button.grid(row=2, column=1, padx=10, pady=10)
 symmetric_frame = LabelFrame(root, text="Simetrično šifriranje - AES")
 symmetric_frame.grid(row=1, column=0, padx=10, pady=10, sticky='nsew')
 
-generate_aes_key_button = Button(symmetric_frame, text="Kreirati AES ključ", command=generate_aes_key)
+generate_aes_key_button = Button(symmetric_frame, text="Kreiraj AES ključ", command=generate_aes_key)
 generate_aes_key_button.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
 symmetric_file_label = Label(symmetric_frame, text="Odaberi datoteku za šifriranje/dešifriranje:")
@@ -262,7 +285,7 @@ def hash_browse_file():
 hash_browse_button = Button(hash_frame, text="Pretraži", command=hash_browse_file)
 hash_browse_button.grid(row=0, column=1)
 
-hash_create_button = Button(hash_frame, text="Kreiraj sažetak", command=lambda: print(save_hash_to_file(hash_file_path.get())))
+hash_create_button = Button(hash_frame, text="Kreiraj sažetak", command=lambda: create_hash(hash_file_path.get()))
 hash_create_button.grid(row=0, column=2, padx=10, pady=10)
 
 sign_frame = LabelFrame(root, text="Potpisivanje poruke")
@@ -295,10 +318,22 @@ def v_original_browse_file():
     file_path = filedialog.askopenfilename()
     v_original_file_path.set(file_path)
 
-v_original_browse_button = Button(v_sign_frame, text="Pretraži", command=v_original_browse_file)
-v_original_browse_button.grid(row=1, column=1)
+v_original_browse_button = Button(v_sign_frame, text="Izvorna datoteka", command=v_original_browse_file)
+v_original_browse_button.grid(row=1, column=2)
 
-v_signature_create_button = Button(v_sign_frame, text="Ovjeri potpis", command=lambda: verify_signature('javni_kljuc.txt', 'text.txt.sig', v_original_file_path.get()))
-v_signature_create_button.grid(row=1, column=2, padx=10, pady=10)
+v_signed_file_label = Label(v_sign_frame, text="Odaberi potpisanu datoteku:")
+v_signed_file_label.grid(row=2, column=0, padx=10, pady=10)
+
+v_signed_file_path = StringVar()
+
+def v_signed_browse_file():
+    file_path = filedialog.askopenfilename()
+    v_signed_file_path.set(file_path)
+
+v_signed_browse_button = Button(v_sign_frame, text="Potpisana datoteka", command=v_signed_browse_file)
+v_signed_browse_button.grid(row=2, column=2)
+
+v_signature_create_button = Button(v_sign_frame, text="Ovjeri potpis", command=lambda: verify_signature('javni_kljuc.txt', v_signed_file_path.get(), v_original_file_path.get()))
+v_signature_create_button.grid(row=3, column=2, padx=10, pady=10)
 
 root.mainloop()
