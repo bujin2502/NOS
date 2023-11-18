5af15fd8076125cc53c096d4bea18df0a39b72d1
Zlatko Pračić <53391918+bujin2502@users.noreply.github.com>
2023-11-18 17:18:34 +0100

    Update pracic.py

--- pracic.py
+++ pracic.py
@@ -17,7 +17,7 @@ import base64
 def generate_rsa_keys():
     private_key = rsa.generate_private_key(
         public_exponent=65537,
-        key_size=2048,
+        key_size=4096,
         backend=default_backend()
     )
     public_key = private_key.public_key()
